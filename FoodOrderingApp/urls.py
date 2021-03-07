877753f597591c85fa31f9b48c603479c12625c2
sSharkyYy <roland.szeghy@gmail.com>
2021-03-07 14:08:49 +0100

    Home page

--- FoodOrderingApp/urls.py
+++ FoodOrderingApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from personal.views import HomeScreenView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeScreenView.as_view(), name='home'),
 ]
