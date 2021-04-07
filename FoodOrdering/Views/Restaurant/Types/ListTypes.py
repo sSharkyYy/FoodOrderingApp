e77598482371f9bd7080ea6acfbd63aa593fbca1
mi <mi@mi-tech.hu>
2021-04-08 00:57:12 +0200

    update: csak a saját kategóriákat listázza étteremnél

--- FoodOrdering/Views/Restaurant/Types/ListTypes.py
+++ FoodOrdering/Views/Restaurant/Types/ListTypes.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.views.generic import ListView
 
@@ -6,10 +7,10 @@ from FoodOrdering.services.ProfileService import ProfileService
 from personal.models import Types
 
 
-class ListTypes(ListView):
+class ListTypes(LoginRequiredMixin, ListView):
     model = Types
     template_name = 'FoodOrdering/Restaurant/Types/list_types.html'
     context_object_name = 'types'
 
     def get_queryset(self):
-        return super().get_queryset().all()
+        return super().get_queryset().filter(owner=ProfileService.get_restaurant_profile(self.request.user))
