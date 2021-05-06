fd870c8408df6adcb8c0c999a12fc4a5eb414408
mi <mi@mi-tech.hu>
2021-05-06 22:13:23 +0200

    fix: megrendeléskor ha átvétel van akkor cím nem kötelező

--- FoodOrdering/Forms/CreateOrder.py
+++ FoodOrdering/Forms/CreateOrder.py
@@ -1,9 +1,18 @@
+from django.core.exceptions import ValidationError
+
 from django.forms import forms, EmailField, CharField, ChoiceField
 
 
 class CreateOrderForm(forms.Form):
     shipping = ChoiceField(choices=((1, 'Házhozszállítás'), (2, 'Átvétel')))
     phone_number = CharField(max_length=10)
-    address = CharField(required=True, max_length=254)
+    address = CharField(required=False, max_length=254)
     name = CharField(required=True, max_length=254)
 
+    def clean(self):
+        results = super().clean()
+        shipping = results.get('shipping', None)
+        address = results.get('address', None)
+        if shipping == 1 and address is None:
+            raise ValidationError('Cím kitöltése kötelező házhozszállítás esetén')
+        return results
--- FoodOrdering/static/FoodOrdering/js/choose.js
+++ FoodOrdering/static/FoodOrdering/js/choose.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     console.log($(this).val());
     if ($(this).val() == 2) {
       $('#div_id_address').hide();
-      $('#div_id_address').removeAttr('required');
+      $('#id_address').removeAttr('required');
       $('#div_id_address').removeAttr('data-error');
       console.log($('#div_id_address'))
     } else {
       $('#div_id_address').show();
-      $('#div_id_address').attr('required', '');
+      $('#id_address').attr('required', '');
       $('#div_id_address').attr('data-error', 'This field is required.');
     }
   });
