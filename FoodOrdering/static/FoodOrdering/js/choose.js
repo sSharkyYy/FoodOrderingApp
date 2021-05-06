b71ec5d75c9c0635886922a3dedcea53204cbb53
mi <mi@mi-tech.hu>
2021-05-06 22:01:58 +0200

    fix

--- FoodOrdering/static/FoodOrdering/js/choose.js
+++ FoodOrdering/static/FoodOrdering/js/choose.js
@@ -1,19 +1,18 @@
-$(document).ready(function(){
-    $("#seeAnotherField").change(function() {
-  if ($(this).val() == "Átvétel") {
-    $('#hazhozDiv').hide();
-    $('#hazhoz').removeAttr('required');
-    $('#hazhoz').removeAttr('data-error');
-
-  } else {
-    $('#hazhozDiv').show();
-    $('#hazhoz').attr('required', '');
-    $('#hazhoz').attr('data-error', 'This field is required.');
-  }
-});
-$("#seeAnotherField").trigger("change");
-
-
+$(document).ready(function () {
+  $("#id_shipping").change(function () {
+    console.log($(this).val());
+    if ($(this).val() == 2) {
+      $('#div_id_address').hide();
+      $('#div_id_address').removeAttr('required');
+      $('#div_id_address').removeAttr('data-error');
+      console.log($('#div_id_address'))
+    } else {
+      $('#div_id_address').show();
+      $('#div_id_address').attr('required', '');
+      $('#div_id_address').attr('data-error', 'This field is required.');
+    }
+  });
+  $("#id_shipping").trigger("change");
 
 
 });
